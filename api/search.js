0d24ddf9d94cd43f3e6c39b9586d1d62d4819d45
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-11-11 13:12:34 -0600

    Update search.js

--- api/search.js
+++ api/search.js
@@ -5,22 +5,19 @@ export default async (req, res) => {
 
     let auth = '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'
 
-    function getPackage() {
-        fetch('https://api.github.com/repos/ThatError404/PyPM/contents/api/pkgs/' + name + '/package.json', {
-            method: 'GET',
-            headers: {
-                'Authorization': 'token ' + auth
-            }
-        }).then(data => {
-            if (data.status == 404) {
-                res.status(404).json({
-                    error: 'Package not found'
-                })
-            } else {
-            }
-        })
-
-    }
-
-    getPackage()
+    fetch('https://api.github.com/repos/ThatError404/PyPM/contents/api/pkgs/' + name + '/package.json', {
+        method: 'GET',
+        headers: {
+            'Authorization': 'token ' + auth
+        }
+    }).then(data => {
+        if (data.status == 404) {
+            res.status(404).json({
+                error: 'Package not found'
+            })
+            return;
+        } else {
+        }
+    })
+    
 };
