c82a9119abeaf8a7847629e6d4753ad7c12f6f83
Error 404 <92172873+ThatError404@users.noreply.github.com>
2022-11-10 19:22:54 -0600

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,10 +1,31 @@
 export default async (req, res) => {
     const name = req.query.name;
 
+    const rest = require('@octokit/rest');
+
+    const octokit = new rest.Octokit({
+        auth: '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',
+    });
+
+
     function getPackage() {
+        octokit.repos.getContent({
+            owner: 'ThatError404',
+            repo: 'PyPM',
+            path: `api/pkgs/${name}/package.json`
+        }).then(({ data }) => {
+            if (data.status === 404) {
+                return false
+            } else {
+                return true
+            }
+        })
+
     }
 
-    if (getPackage() == null) {
+    if (getPackage(name) === false) {
         res.status(404).send(`Package '${name}' not found.`);
+    } else {
+        res.status(200).send(`Package '${name}' found.`);
     }
 };
